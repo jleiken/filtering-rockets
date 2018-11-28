7ca88ada5eb3ac1eb7f4efc1e5445a87834e44f6
Jacob Leiken <jacobleiken@gmail.com>
2018-11-27 23:25:47 -0500

    Rebranded but totally broken

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-import HelloWorld from './HelloWorld';
-import Counter from './Counter';
 import FilteredList from './FilteredList';
 
-const produce = [
-	{name: "Apple", type: "Fruit"},
-	{name: "Pineapple", type: "Fruit"},
-	{name: "Banana", type: "Fruit"},
-	{name: "Pear", type: "Fruit"},
-	{name: "Strawberry", type: "Fruit"},
-	{name: "Orange", type: "Fruit"},
-	{name: "Lettuce", type: "Vegetable"},
-	{name: "Cucumber", type: "Vegetable"},
-	{name: "Eggplant", type: "Vegetable"},
-	{name: "Squash", type: "Vegetable"},
-	{name: "Bell pepper", type: "Vegetable"},
-	{name: "Onion", type: "Vegetable"}
+const rockets = [
+	{name: "Antares", org: "Orbital ATK", capacity: 5000},
+	{name: "Atlas V", org: "ULA", capacity: 20520},
+	{name: "Electron", org: "Rocket Lab", capacity: 250},
+	{name: "Falcon 9", org: "SpaceX", capacity: 22800},
+	{name: "Falcon Heavy", org: "SpaceX", capacity: 63800},
+	{name: "BFR", org: "SpaceX", capacity: 100000},
+	{name: "Pegasus", org: "Orbital ATK", capacity: 443},
+	{name: "Redstone", org: "US Army", capacity: 2860},
+	{name: "Saturn I", org: "NASA", capacity: 9070},
+	{name: "Saturn V", org: "NASA", capacity: 140000},
+	{name: "Space Shuttle", org: "NASA", capacity: 27500},
+	{name: "Space Launch System", org: "ULA", capacity: 117934},
+	{name: "Delta IV", org: "ULA", capacity: 11470},
+	{name: "Delta IV Heavy", org: "ULA", capacity: 28790},
+	{name: "Vulcan", org: "ULA", capacity: 34900},
+	{name: "Taurus", org: "Northrop Grumman", capacity: 1320},
+	{name: "New Glenn", org: "Blue Origin", capacity: 45000}
 ];
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<HelloWorld name={`Jacob`} />
-				<Counter />
-				<FilteredList items={produce} />
+				<FilteredList items={rockets} />
 			</div>
 		);
 	}
--- src/FilteredList.jsx
+++ src/FilteredList.jsx
@@ -1,48 +1,66 @@
 import React, { Component } from 'react';
 import { DropdownButton, MenuItem } from 'react-bootstrap';
-import List from './List';
+import Grid from './Grid';
 
 class FilteredList extends Component {
-  constructor(props) {
-	super(props);
+	constructor(props) {
+		super(props);
 
-	//The state is just a list of key/value pairs (like a hashmap)
-	this.state = {
-	  search: "",
-	  type: "All"
-	};
-  }
+		// The state is just a list of key/value pairs (like a hashmap)
+		this.state = {
+			search: "",
+			type: "All"
+		};
+	}
 
-  //Sets the state whenever the user types on the search bar
-  onSearch = (event) => {
-	this.setState({search: event.target.value.trim().toLowerCase()});
-  }
+	// Sets the state whenever the user types on the search bar
+	onSearch = (event) => {
+		this.setState({search: event.target.value.trim().toLowerCase()});
+	}
 
-  //Sets the state of the "type" state variable depending on what is passed in
-  onFilter = (event) => {
-	this.setState({type: event});
-  }
+	// Sets the state of the "type" state variable depending on what is passed in
+	onFilter = (event) => {
+		this.setState({type: event});
+	}
 
-  //Changes filterItem to take into account the "type" state variable when filtering
-  filterItem = (item) => {
-	  return item.name.toLowerCase().search(this.state.search) !== -1 
-	  	&& (this.state.type === "All" || item.type === this.state.type);
-  }
+	// Changes filterItem to take into account the "type" state variable when filtering
+	filterItem = (item) => {
+		return item.name.toLowerCase().search(this.state.search) !== -1 
+			&& (this.state.type === "All" || item.type === this.state.type);
+	}
 
-  render(){
-	return (
-		<div className = "filter-list">	
-			<h1>Produce Search</h1>
-			<DropdownButton id="typeDropdown" title={"Type"}>
-				<MenuItem eventKey="All" onSelect={this.onFilter}>All</MenuItem>
-				<MenuItem eventKey="Fruit" onSelect={this.onFilter}>Fruits</MenuItem>
-				<MenuItem eventKey="Vegetable" onSelect={this.onFilter}>Vegetables</MenuItem>
-			</DropdownButton>
-			<input type = "text" placeholder = "Search" onChange = {this.onSearch} />
-			<List items = {this.props.items.filter(this.filterItem)} />
-		</div>
-	);
-  }
+	render(){
+		return (
+			<div className = "filter-list">	
+				<h1>American Rockets</h1>
+
+				<DropdownButton id="typeDropdown" title={"Filter by organization"}>
+					<MenuItem eventKey="All" onSelect={this.onFilter}>All</MenuItem>
+					<MenuItem eventKey="NASA" onSelect={this.onFilter}>NASA</MenuItem>
+					<MenuItem eventKey="ULA" onSelect={this.onFilter}>ULA</MenuItem>
+					<MenuItem eventKey="SpaceX" onSelect={this.onFilter}>SpaceX</MenuItem>
+					<MenuItem eventKey="Other" onSelect={this.onFilter}>Other</MenuItem>
+				</DropdownButton>
+
+				<DropdownButton id="typeDropdown" title={"Filter by payload capacity"}>
+					<MenuItem eventKey="all" onSelect={this.onFilter}>All</MenuItem>
+					<MenuItem eventKey="small" onSelect={this.onFilter}>{"<"}200 kg</MenuItem>
+					<MenuItem eventKey="medium" onSelect={this.onFilter}>{">"}200 kg or {"<"}500 kg</MenuItem>
+					<MenuItem eventKey="large" onSelect={this.onFilter}>{">"}500 kg</MenuItem>
+				</DropdownButton>
+
+				<input type = "text" placeholder = "Search" onChange = {this.onSearch} />
+
+				<DropdownButton id="typeDropdown" title={"Sort"}>
+					<MenuItem eventKey="Alphabetical" onSelect={this.onSort}>Alphabetical</MenuItem>
+					<MenuItem eventKey="Reverse alphabetical" onSelect={this.onSort}>Reverse alphabetical</MenuItem>
+					<MenuItem eventKey="Payload capacity" onSelect={this.onSort}>Payload capacity</MenuItem>
+				</DropdownButton>
+
+				<Grid items = {this.props.items.filter(this.filterItem)} />
+			</div>
+		);
+	}
 }
 
 export default FilteredList;
